5b888570cf03191b9c49d18c396b8a509961c97f
Jan Winkelmann <j-winkelmann@tuhh.de>
2017-01-29 03:35:25 +0100

    move error stuff to error.go

--- response.go
+++ response.go
@@ -10,26 +10,6 @@ import (
 	"strings"
 )
 
-// ErrorType signfies a category of errors
-type ErrorType uint
-
-// ErrorTypes convey what category of error ocurred
-const (
-	ErrNormal         ErrorType = iota // general errors
-	ErrClient                          // error was caused by the client, (e.g. invalid CLI usage)
-	ErrImplementation                  // programmer error in the server
-	// TODO: add more types of errors for better error-specific handling
-)
-
-// Error is a struct for marshalling errors
-type Error struct {
-	Message string
-	Code    ErrorType
-}
-
-func (e Error) Error() string {
-	return e.Message
-}
 
 // EncodingType defines a supported encoding
 type EncodingType string
