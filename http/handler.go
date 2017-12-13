711265187a52856c66d88802bd056e5c86e02e99
Steven Allen <steven@stebalien.com>
2017-12-12 16:07:04 -0800

    don't wait for command if it returns an error

--- http/handler.go
+++ http/handler.go
@@ -193,6 +193,7 @@ func (i internalHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	err = i.root.Call(req, re)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	d := re.(Doner)
