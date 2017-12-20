471e0f4183947b1bbcac33af461ede737baed7ca
keks <keks@cryptoscope.co>
2017-12-20 20:56:07 +0100

    more thorough JSON (un)marshaling tests

--- json_test.go
+++ json_test.go
@@ -6,52 +6,90 @@ import (
 )
 
 func TestMarshal(t *testing.T) {
-	e := Error{
-		Message: "error msg",
+	type testcase struct {
+		msg  string
+		code ErrorType
 	}
 
-	buf, err := json.Marshal(e)
-	if err != nil {
-		t.Fatal(err)
+	tcs := []testcase{
+		{msg: "error msg", code: 0},
+		{msg: "error msg", code: 1},
+		{msg: "some other error msg", code: 1},
 	}
 
-	m := make(map[string]interface{})
+	for _, tc := range tcs {
+		e := Error{
+			Message: tc.msg,
+			Code:    tc.code,
+		}
 
-	err = json.Unmarshal(buf, &m)
-	if err != nil {
-		t.Fatal(err)
+		buf, err := json.Marshal(e)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		m := make(map[string]interface{})
+
+		err = json.Unmarshal(buf, &m)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(m) != 3 {
+			t.Errorf("expected three map elements, got ", len(m))
+		}
+
+		if m["Message"].(string) != tc.msg {
+			t.Errorf(`expected m["Message"] to be %q, got %q`, tc.msg, m["Message"])
+		}
+
+		icode := ErrorType(m["Code"].(float64))
+		if icode != tc.code {
+			t.Errorf(`expected m["Code"] to be %v, got %v`, tc.code, icode)
+		}
+
+		if m["Type"].(string) != "error" {
+			t.Errorf(`expected m["Type"] to be %q, got %q`, "error", m["Type"])
+		}
+	}
+}
+
+func TestUnmarshal(t *testing.T) {
+	type testcase struct {
+		json string
+		msg  string
+		code ErrorType
+
+		err string
 	}
 
-	if len(m) != 3 {
-		t.Fatal("expected three map elements, got ", len(m))
+	tcs := []testcase{
+		{json: `{"Message":"error msg","Code":0}`, msg: "error msg", err: "not of type error"},
+		{json: `{"Message":"error msg","Code":0,"Type":"error"}`, msg: "error msg"},
+		{json: `{"Message":"error msg","Code":1,"Type":"error"}`, msg: "error msg", code: 1},
+		{json: `{"Message":"some other error msg","Code":1,"Type":"error"}`, msg: "some other error msg", code: 1},
 	}
 
-	if m["Message"].(string) != "error msg" {
-		t.Fatal(`expected m["Message"] == "error msg", got "`, m["Message"], `"`)
-	}
+	for i, tc := range tcs {
+		t.Log("at test case", i)
+		var e Error
+		err := json.Unmarshal([]byte(tc.json), &e)
+		if err != nil && err.Error() != tc.err {
+			t.Errorf("expected parse error %q but got %q", tc.err, err)
+		} else if err == nil && tc.err != "" {
+			t.Errorf("expected parse error %q but got %q", tc.err, err)
+		}
 
-	if m["Code"].(float64) != 0 {
-		t.Fatal(`expected m["Code"] == 0, got "`, m["Code"], `"`)
-	}
+		if err != nil {
+			continue
+		}
 
-	if m["Type"].(string) != "error" {
-		t.Fatal(`expected m["Type"] == "error", got "`, m["Type"], `"`)
-	}
+		if e.Message != tc.msg {
+			t.Errorf("expected e.Message to be %q, got %q", tc.msg, e.Message)
+		}
 
-	e = Error{}
-	t.Logf("%s\n", buf)
-
-	err = json.Unmarshal(buf, &e)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("%#v\n", e)
-
-	if e.Message != "error msg" {
-		t.Fatal(`expected e.Message == "error msg", got "` + e.Message + `"`)
-	}
-
-	if e.Code != 0 {
-		t.Fatal(`expected e.Code == 0, got "`, e.Code, `"`)
+		if e.Code != tc.code {
+			t.Errorf("expected e.Code to be %q, got %q", tc.code, e.Code)
+		}
 	}
 }
