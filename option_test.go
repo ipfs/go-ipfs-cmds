8a4e361a70041dad2689d2b3050ae55a7d6df05e
Steven Allen <steven@stebalien.com>
2019-05-10 18:24:40 -0700

    chore: remove some dead code

--- option_test.go
+++ option_test.go
@@ -7,32 +7,6 @@ import (
 	"testing"
 )
 
-func TestOptionValueExtractBoolNotFound(t *testing.T) {
-	t.Log("ensure that no error is returned when value is not found")
-	optval := &OptionValue{ValueFound: false}
-	_, _, err := optval.Bool()
-	if err != nil {
-		t.Fatal("Found was false. Err should have been nil")
-	}
-}
-
-func TestOptionValueExtractWrongType(t *testing.T) {
-
-	t.Log("ensure that error is returned when value if of wrong type")
-
-	optval := &OptionValue{Value: "wrong type: a string", ValueFound: true}
-	_, _, err := optval.Bool()
-	if err == nil {
-		t.Fatal("No error returned. Failure.")
-	}
-
-	optval = &OptionValue{Value: "wrong type: a string", ValueFound: true}
-	_, _, err = optval.Int()
-	if err == nil {
-		t.Fatal("No error returned. Failure.")
-	}
-}
-
 func TestLackOfDescriptionOfOptionDoesNotPanic(t *testing.T) {
 	opt := BoolOption("a", "")
 	opt.Description()
